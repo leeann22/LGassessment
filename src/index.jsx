f3d99c86600f7c71fd37de0d59ae293f76580b4d
leeann22 <126453766+leeann22@users.noreply.github.com>
2024-08-14 01:59:49 +0800

    Updated index.jsx to include Routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 /**
  * This file can be ignored, please work in ./components/App.jsx
@@ -14,9 +15,18 @@ import './styles/index.css';
 // Include application component.
 import App from './components/App';
 
+import Details from './components/Details';
+import PostDetails from './components/PostDetails';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/details" element={<Details />} />
+          <Route path="/posts/:postId" element={<PostDetails />} />
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
